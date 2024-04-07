c9dcfeb1cbe133e090335537edaa466385a4b500
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-04-07 18:14:13 +0300

    .

---
+++ src/types/db.d.ts
@@ -0,0 +1,19 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  image: string;
+}
+
+interface Chat {
+  id: string;
+  messages: Message[];
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  receiverId: string;
+  text: string;
+  timestamp: number;
+}
